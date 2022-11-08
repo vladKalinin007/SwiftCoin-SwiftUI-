2667366facfef6f4438a855f22ecc2ea8b807e81
KaliNinB <109882612+KaliNinB@users.noreply.github.com>
2022-11-08 07:22:49 +0200

    Initial Commit

---
+++ SwiftCoin/SwiftCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinApp.swift
+//  SwiftCoin
+//
+//  Created by Владислав Калинин on 08.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
